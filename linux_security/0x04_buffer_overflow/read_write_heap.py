bdc2989c2618812c870045d6fe7508fb8d0c2992
Ethan1709 <benyayerethan@gmail.com>
2024-05-22 14:34:43 +0200

    fix pep8

--- linux_security/0x04_buffer_overflow/read_write_heap.py
+++ linux_security/0x04_buffer_overflow/read_write_heap.py
@@ -10,7 +10,8 @@ def exploit_heap(pid, search_str, replace_str):
     search_str = search_str.encode('utf-8')
     replace_str = replace_str.encode('utf-8')
 
-    info = subprocess.check_output(f"cat /proc/{pid}/maps | grep 'heap'", shell=True)
+    info = subprocess.check_output(
+        f"cat /proc/{pid}/maps | grep 'heap'", shell=True)
     info = info.decode('utf-8')
     info = info.split(' ')
 
@@ -39,9 +40,12 @@ def exploit_heap(pid, search_str, replace_str):
             os.write(mem_file, b'\x00' * len(search_str))
             os.lseek(mem_file, abs_address, os.SEEK_SET)
             os.write(mem_file, replace_str)
-            print(f"Replaced '{search_str.decode()}' with '{replace_str.decode()}' at address {hex(abs_address)}")
+            print(
+                f"Replaced '{search_str.decode()}' with
+                '{replace_str.decode()}' at address {hex(abs_address)}")
     finally:
         os.close(mem_file)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
