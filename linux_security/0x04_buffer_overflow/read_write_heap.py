87b4482a9747545e4f572cfb9aa58b76ab2ebcd5
Ethan1709 <benyayerethan@gmail.com>
2024-05-22 14:11:06 +0200

    add if name == main

--- linux_security/0x04_buffer_overflow/read_write_heap.py
+++ linux_security/0x04_buffer_overflow/read_write_heap.py
@@ -1,49 +1,54 @@
 #!/usr/bin/python3
 """heap exploitation"""
 
-
 import os
 import sys
 import subprocess
 
+def exploit_heap(pid, search_str, replace_str):
+    search_str = search_str.encode('utf-8')
+    replace_str = replace_str.encode('utf-8')
 
-pid = int(sys.argv[1])
-search_str = sys.argv[2].encode('utf-8')
-replace_str = sys.argv[3].encode('utf-8')
+    info = subprocess.check_output(f"cat /proc/{pid}/maps | grep 'heap'", shell=True)
+    info = info.decode('utf-8')
+    info = info.split(' ')
 
-if len(sys.argv) != 4:
-    print(f'Error: usage pid, string_to_find, replacing_string')
-    sys.exit(1)
+    addresses = info[0]
+    addresses_split = addresses.split('-')
+    start_addr = addresses_split[0]
+    start_addr = int(start_addr, 16)
+    end_addr = addresses_split[1]
+    end_addr = int(end_addr, 16)
 
-info = subprocess.check_output(f"cat /proc/{pid}/maps | grep 'heap'", shell=True)
-info = info.decode('utf-8')
-info = info.split(' ')
+    heap_start = start_addr
+    heap_end = end_addr
+    heap_size = heap_end - heap_start
 
-addresses = info[0]
-addresses_split = addresses.split('-')
-start_addr = addresses_split[0]
-start_addr = int(start_addr, 16)
-end_addr = addresses_split[1]
-end_addr = int(end_addr, 16)
+    mem_path = f"/proc/{pid}/mem"
+    mem_file = os.open(mem_path, os.O_RDWR)
+    try:
+        os.lseek(mem_file, heap_start, os.SEEK_SET)
+        heap_data = os.read(mem_file, heap_size)
+        offset = heap_data.find(search_str)
+        if offset == -1:
+            print(f"String {search_str} not found in the heap.")
+        else:
+            abs_address = heap_start + offset
+            os.lseek(mem_file, abs_address, os.SEEK_SET)
+            os.write(mem_file, b'\x00' * len(search_str))
+            os.lseek(mem_file, abs_address, os.SEEK_SET)
+            os.write(mem_file, replace_str)
+            print(f"Replaced '{search_str.decode()}' with '{replace_str.decode()}' at address {hex(abs_address)}")
+    finally:
+        os.close(mem_file)
 
-heap_start = start_addr
-heap_end = end_addr
-heap_size = heap_end - heap_start
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print(f'Error: usage pid, string_to_find, replacing_string')
+        sys.exit(1)
 
-mem_path = f"/proc/{pid}/mem"
-mem_file = os.open(mem_path, os.O_RDWR)
-try:
-    os.lseek(mem_file, heap_start, os.SEEK_SET)
-    heap_data = os.read(mem_file, heap_size)
-    offset = heap_data.find(search_str)
-    if offset == -1:
-        print(f"String {search_str} not found in the heap.")
-    else:
-        abs_address = heap_start + offset
-        os.lseek(mem_file, abs_address, os.SEEK_SET)
-        os.write(mem_file, b'\x00' * len(search_str))
-        os.lseek(mem_file, abs_address, os.SEEK_SET)
-        os.write(mem_file, replace_str)
-        print(f"Replaced '{search_str.decode()}' with '{replace_str.decode()}' at address {hex(abs_address)}")
-finally:
-    os.close(mem_file)
+    pid = int(sys.argv[1])
+    search_str = sys.argv[2]
+    replace_str = sys.argv[3]
+
+    exploit_heap(pid, search_str, replace_str)
