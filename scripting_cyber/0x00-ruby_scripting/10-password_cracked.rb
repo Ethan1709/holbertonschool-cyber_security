7dfb25574772d6f519e8d5b6f889423679b75743
Ethan1709 <benyayerethan@gmail.com>
2024-07-08 21:26:37 +0200

    add task 10

---
+++ scripting_cyber/0x00-ruby_scripting/10-password_cracked.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/ruby
+
+
+require 'digest'
+
+def crack(hashed_password, dictionary_file)
+  if ARGV.length != 2
+    puts 'Usage: 10-password_cracked.rb HASHED_PASSWORD DICTIONARY_FILE'
+    exit
+  end
+
+  File.foreach("dict.txt") { |line|
+  cleaned_line = line.chomp
+  sha256= Digest::SHA256.hexdigest(cleaned_line)
+  if sha256 == hashed_password
+    puts "Password found: #{cleaned_line}"
+    exit
+  end
+  }
+
+  puts 'Password not found in dictionary.'
+end
+
+hashed_password = ARGV[0]
+dictionary_file = ARGV[1]
+
+crack(hashed_password, dictionary_file)
