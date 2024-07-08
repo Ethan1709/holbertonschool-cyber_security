037c8a1c522014b63a2b6334c6ecda28beca5d7f
Ethan1709 <benyayerethan@gmail.com>
2024-07-08 22:55:53 +0200

    change cond

--- scripting_cyber/0x00-ruby_scripting/6-get.rb
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -5,17 +5,11 @@ require 'uri'
 
 def get_request(url)
   uri = URI(url)
-
-  begin
-    res = res = Net::HTTP.get_response(uri)
-    if res.code == '200'
-      puts "Response status: #{res.code} OK"
-      puts 'Response body:'
-      puts res.body
-    end
-  rescue StandardError => e
-    puts "Response status: 404 Not Found"
-    puts 'Response body:'
-    puts "{}"
+  res = res = Net::HTTP.get_response(uri)
+  if res.code != '200'
+    then puts "Response status: #{res.code} Not Found"
+    else puts "Response status: #{res.code} OK"
   end
+  puts 'Response body:'
+  puts res.body
 end
