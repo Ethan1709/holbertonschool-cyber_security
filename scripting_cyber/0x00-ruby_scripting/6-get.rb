6e0fb8cd555e5d77c00c137d0aacb54588eacc75
Ethan1709 <benyayerethan@gmail.com>
2024-07-08 19:28:05 +0200

    add task 8

--- scripting_cyber/0x00-ruby_scripting/6-get.rb
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -6,8 +6,8 @@ require 'uri'
 def get_request(url)
   uri = URI(url)
   res = res = Net::HTTP.get_response(uri)
-  puts res.code
+  puts "Response status: #{res.code} OK"
+  puts 'Response body:'
   puts res.body
 end
 
-url = 'https://jsonplaceholder.typicode.com/posts/1'
