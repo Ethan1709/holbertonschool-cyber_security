81a49e3e6a918a243bf079f42883594700126f30
Ethan1709 <benyayerethan@gmail.com>
2024-07-08 14:35:54 +0200

    add task 2 removing the class

--- scripting_cyber/0x00-ruby_scripting/2-prime.rb
+++ scripting_cyber/0x00-ruby_scripting/2-prime.rb
@@ -1,11 +1,9 @@
 #!/usr/bin/ruby
 
-class Prime
-    def prime(number)
-        return false if number <= 1
-        for i in 2...number do
-            return false if number % i == 0
-        end
-        return true
+ def prime(number)
+    return false if number <= 1
+    for i in 2...number do
+        return false if number % i == 0
     end
+    return true
 end
