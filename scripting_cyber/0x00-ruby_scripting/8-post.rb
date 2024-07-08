8f39f0a3dfa7f8ff278d581b2627173dac55e0b1
Ethan1709 <benyayerethan@gmail.com>
2024-07-08 19:37:56 +0200

    add quotes

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -6,7 +6,10 @@ require 'uri'
 def post_request(url, body_params)
   uri = URI(url)
   res = Net::HTTP.post_form(uri, body_params)
-  puts "Response status: #{res.code} Created"
+  if res.code != '201'
+    then puts "Response status: #{res.code} Not Found"
+    else puts "Response status: #{res.code} Created"
+  end
   puts 'Response body:'
   puts res.body
 end
