f7e35e3b86103743376e9c13c2b78164180dc3ab
Ethan1709 <benyayerethan@gmail.com>
2024-07-08 22:37:15 +0200

    add check 404

--- scripting_cyber/0x00-ruby_scripting/6-get.rb
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -6,8 +6,10 @@ require 'uri'
 def get_request(url)
   uri = URI(url)
   res = res = Net::HTTP.get_response(uri)
-  puts "Response status: #{res.code} OK"
+  if res.code != '200'
+    puts "Response status: #{res.code} Not found"
+    exit
+  puts puts "Response status: #{res.code} OK"
   puts 'Response body:'
   puts res.body
 end
-
