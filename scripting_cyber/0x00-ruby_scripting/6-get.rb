14e74b3b0e84b384a0645e21ab74c4391dd0253f
Ethan1709 <benyayerethan@gmail.com>
2024-07-08 22:51:37 +0200

    add rescue

--- scripting_cyber/0x00-ruby_scripting/6-get.rb
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -5,11 +5,18 @@ require 'uri'
 
 def get_request(url)
   uri = URI(url)
-  res = res = Net::HTTP.get_response(uri)
-  if res.code != '200'
-    then puts "Response status: #{res.code} Not found"
-    else puts "Response status: #{res.code} OK"
+
+  begin
+    res = res = Net::HTTP.get_response(uri)
+    if res.code != '200'
+      then puts "Response status: #{res.code} Not found"
+      else puts "Response status: #{res.code} OK"
+    end
+    puts 'Response body:'
+    puts res.body
+  rescue StandardError => e
+    puts "Response status: 404 Not found"
+    puts 'Response body:'
+    puts "{}"
   end
-  puts 'Response body:'
-  puts res.body
 end
