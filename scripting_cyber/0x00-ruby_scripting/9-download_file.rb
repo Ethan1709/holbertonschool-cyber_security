0845594f86941420014d5c8c9e5cc83042df68dc
Ethan1709 <benyayerethan@gmail.com>
2024-07-08 20:20:22 +0200

    add task 9

---
+++ scripting_cyber/0x00-ruby_scripting/9-download_file.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/ruby
+
+
+require "open-uri"
+require "uri"
+require "fileutils"
+def download(url, path)
+  if ARGV.length != 2
+    puts 'Usage: 9-download_file.rb URL LOCAL_FILE_PATH'
+    exit
+  end
+
+  open(url) do |image|
+    File.open("./test.jpg", "wb") do |file|
+      file.write(image.read)
+      FileUtils.mv(file.path, "#{path}")
+      puts "Downloading file from #{url}}..."
+    end
+  end
+
+  puts "File downloaded and saved to #{path}."
+
+
+end
+
+url = ARGV[0]
+path = ARGV[1]
+download(url, path)
